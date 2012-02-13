b2bbba287f19a87c46388e1095ebfdadf81cbbf2
Brian Rowe <briprowe@yahoo.com>
2012-02-13 11:49:55 -0500

    Add some functions from other projects.

--- src/clj_util/core.clj
+++ src/clj_util/core.clj
@@ -2,6 +2,40 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
+
+(defn encode-utf8
+  [str]
+  (.getBytes str "UTF8"))
+
+(defn decode-utf8
+  [bytes]
+  (String. bytes "UTF8"))
+
+(defn byte-cast [x]
+  (let [y (bit-and 16r000000ff x)]
+    (byte (if (> y 127) (+ -256 y) y))))
+
+(defn to-bytes
+  [seq]
+  (byte-array (count seq) (map byte-cast seq)))
+
+(let [values {\0 0, \1 1, \2 2, \3 3, \4 4, \5 5, \6 6,
+              \7 7, \8 8, \9 9, \a 0xa, \b 0xb, \c 0xc,
+              \d 0xd, \e 0xe, \f 0xf}]
+  (defn make-byte
+    "Return a java byte given two hexidecimal characters."
+    [char1 char2]
+    {:pre [(comp not nil? (get values char1))
+           (comp not nil? (get values char2))]
+     :post [(instance? java.lang.Byte %)]}
+    (byte-cast (+ (* 16 (get values char1)) (get values char2)))))
+
+(defn make-byte-array
+  "Make a byte array given a hexidecimal string."
+  [s]
+  (byte-array (map #(make-byte (first %) (second %))
+                   (partition 2 s))))
+
 (defn read-key-value-stream
   "Read a stream consisting of a series of \"key=value\n\" and
 return a map containing those pairs. "
