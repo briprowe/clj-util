abecc0252c14423a93c4c6212cf2eea84d467683
Brian Rowe <briprowe@yahoo.com>
2012-06-03 11:50:17 -0400

    Modify groupname for pushing to clojars.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject clj-util "1.0.0-SNAPSHOT"
+(defproject org.clojars.briprowe/clj-util "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]])
